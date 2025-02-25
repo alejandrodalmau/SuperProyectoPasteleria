06abc66517c0fc8b9e1d584a87085b7eddb6f1a2
alejandro <alejandroedgardodalmau@gmail.com>
2025-02-24 21:23:05 -0300

    inclusion foto

--- src/componentes/Layout/Header.js
+++ src/componentes/Layout/Header.js
@@ -1,6 +1,10 @@
 const Header=(props)=>{
     return(
+        <>
         <h1>LA PAGINA DE REACT-el comienzo-</h1>
+        <img src="otros.jpg"></img>
+        </>
     )
+
 }
 export default Header;
